a543fc2f715f8a23fae7a5291f5c0e67cfa4ac97
quicktypeI <90068805+quicktypeI@users.noreply.github.com>
2021-09-08 14:52:28 -0700

    sharing code .1 Elliot

--- while loop Elliot.py
+++ while loop Elliot.py
@@ -1,21 +1,4 @@
-##def get_Fullname(first_name, last_name):
-##    full_name = first_name + ' ' + last_name
-##    return full_name.title()
-##while True:
-##    print("\nPlease Inupt your name to Continue:")
-##    f_name = input("first name:")
-##    l_name = input("last name:")
-##
-##    formatted_name = get_Fullname(f_name, l_name)
-##    print("welcome " + formatted_name)
-##    if f_name or l_name == 'quit':
-##        break
-##
-##def build_person(hair_c, eye_c):
-##    person = {'hair' : hair_c, 'eyes' : eye_c}
-##    return person
-##red = build_person('Red hair', 'Brown eyes')
-##print(red)
+
 
 def aqquireName(f_name, n_name):
         name_sys = f_name + ', or ' + n_name
@@ -37,8 +20,14 @@ def introF(f_name, n_name):
 friend = introF('Elliot', 'Dot')
 print("My newest friend is named " + friend['name']+ " though they sometimes go as " + friend['nickname'])
 print(friend)
-    
 
+
+    
+def bondT(f_name, n_name):
+        convo = {'name' : f_name, 'nickname' : n_name}
+        return greet
+play = bondT('Elliot', 'Dot')
+print("What are some games you would like to play" + play['name']
 
     
     
